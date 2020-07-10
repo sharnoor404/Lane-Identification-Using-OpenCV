e1b2cc3da1981771dfe8de6b0e7fd2be2f97e5aa
sharnoor404 <sharnoor404@gmail.com>
2020-07-10 12:32:35 +0530

    conversion to gray scale

--- lanes.py
+++ lanes.py
@@ -1,7 +1,11 @@
 import cv2
+import numpy as np
 #returns image as a multi dimensional numpy array containing relative intensity of pixels
 image=cv2.imread('test_image.jpg')
-#first argument is the window where we want to display image, and the second is the image
-cv2.imshow('result',image)
-#displays image for infinite time
+# first argument is the window where we want to display image, and the second is the image
+# STEP 1- CONVERT IMAGE TO GRAY SCALE
+lane_image=np.copy(image)
+gray=cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+cv2.imshow('result',gray)
+# displays image for infinite time
 cv2.waitKey(0)
