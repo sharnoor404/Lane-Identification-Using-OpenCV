c37f6955240f1026279aac7fc2f2f65c2285dfe4
sharnoor404 <sharnoor404@gmail.com>
2020-07-10 12:57:44 +0530

    blur and edge detection

--- lanes.py
+++ lanes.py
@@ -6,6 +6,13 @@ image=cv2.imread('test_image.jpg')
 # STEP 1- CONVERT IMAGE TO GRAY SCALE
 lane_image=np.copy(image)
 gray=cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
-cv2.imshow('result',gray)
+# STEP 2- REDUCE NOISE AND SMOOTHEN IMAGE
+#uisng a kernel filter of 5*5 with 0 deviation
+blur =cv2.GaussianBlur(gray,(5,5),0)
+# STEP 3-GRADIENT IMAGE FOR EDGE DETECTION, by using a lower threshold as 50 and higher threshold as 150(ratio=3)
+#pixels with gradient below lower threshold are rejected, tjose with gradient greater than upper threshold are accepted and marked as edge,
+#and the pixels with gradient between the lower and upper threshold are accepted as edge only if they are connected to a pixel with threshold above upper threshold
+canny=cv2.Canny(blur,50,150)
+cv2.imshow('result',canny)
 # displays image for infinite time
 cv2.waitKey(0)
